0cafd96b83c328869a8dbae31b12db3e7067425a
Prajath <Prajath@DESKTOP-0N5SVMT>
2022-11-22 18:09:50 +0530

    first commit

---
+++ Corejava/src/com/cg/corejava/Datatypedemo.java
@@ -0,0 +1,16 @@
+package com.cg.corejava;
+
+public class Datatypedemo {
+
+	public static void main(String[] args) {
+		
+	    double pi=22/7;
+		double r= 6.80;
+		double area=pi*r*r;
+		System.out.println("Area= "+area);
+		
+		
+
+	}
+
+}
